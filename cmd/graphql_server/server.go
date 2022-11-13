eaaba5c9122009c62cd0e91d13ef3f47d2bfdfce
Tuba <brunogabrielsd@hotmail.com>
2022-11-13 12:30:57 -0300

    feat: implemented category query and mutation

--- cmd/graphql_server/server.go
+++ cmd/graphql_server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/IamP5/go-graphql-demo/internal/database"
 	"log"
 	"net/http"
 	"os"
@@ -9,17 +11,28 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/IamP5/go-graphql-demo/graph"
 	"github.com/IamP5/go-graphql-demo/graph/generated"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
